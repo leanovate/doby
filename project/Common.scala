e06b85ecf51e0f975680177e5a6983fcd9c8eac6
Yann Simon <yann.simon.fr@gmail.com>
2014-05-27 20:16:18 +0200

    release to sonatype

--- project/Common.scala
+++ project/Common.scala
@@ -1,8 +1,24 @@
+import com.typesafe.sbt.pgp.PgpKeys._
+import sbt._
 import sbt.Keys._
+import sbtrelease.ReleasePlugin.ReleaseKeys._
+import sbtrelease.ReleaseStateTransformations._
+import sbtrelease.ReleaseStep
+import xerial.sbt.Sonatype.SonatypeKeys._
 
 object Common {
 
-  val settings = Release.settings ++ Seq(
+  lazy val publisSignedArtifactsAction = {
+    st: State =>
+      val extracted = Project.extract(st)
+      val ref = extracted.get(thisProjectRef)
+      extracted.runAggregated(publishSigned in Global in ref, st)
+  }
+
+  val settings =
+    xerial.sbt.Sonatype.sonatypeSettings ++
+      com.typesafe.sbt.SbtPgp.settings ++
+      Release.settings ++ Seq(
     organization := "de.leanovate.doby",
     scalaVersion := "2.11.0"
   )
