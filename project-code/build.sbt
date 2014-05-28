de87a8dd2c22523079acb195a4d423a173779870
Yann Simon <yann.simon.fr@gmail.com>
2014-05-28 14:44:56 +0200

    WIP

--- project-code/build.sbt
+++ project-code/build.sbt
@@ -3,3 +3,8 @@ name := "doby"
 Common.settings
 
 libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value
+
+libraryDependencies ++= (
+  if (scalaVersion.value.startsWith("2.10")) List("org.scalamacros" %% "quasiquotes" % Common.paradiseVersion)
+  else Nil
+)
--- project/Common.scala
+++ project/Common.scala
@@ -1,11 +1,16 @@
+import sbt._
 import sbt.Keys._
 
 object Common {
 
+  val paradiseVersion = "2.0.0"
+
   val settings =
     Release.settings ++ Seq(
       organization := "de.leanovate.doby",
-      scalaVersion := "2.11.1"
+      scalaVersion := "2.11.1",
+      crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.11.0", "2.11.1"),
+      addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full)
     )
 
 }
